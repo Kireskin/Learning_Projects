5e7f68017cda596a2bd6161676bcf0072a144ace
Kireskin <simonhlousek@gmail.com>
2024-04-02 19:43:03 +0200

    Adding project introducing the reAct LLM Framework

---
+++ react-langchain/callbacks.py
@@ -0,0 +1,20 @@
+from typing import Dict, Any, List
+
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.schema import LLMResult
+
+
+class AgentCallbackHandler(BaseCallbackHandler):
+    @staticmethod
+    def on_llm_start(
+            serialized: Dict[str, Any], prompts: List[str], **kwargs: Any
+    ) -> Any:
+        """Run when LLM starts running."""
+        print(f"***Prompt to LLM was:***\n{prompts[0]}")
+        print("*********")
+
+    @staticmethod
+    def on_llm_end(response: LLMResult, **kwargs: Any) -> Any:
+        """Run when LLM ends running."""
+        print(f"***LLM Response:***\n{response.generations[0][0].text}")
+        print("*********")
